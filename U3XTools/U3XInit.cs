d8af7c2057abbb1439025d43e000a81d91313df2
Cyphierion <35496928+Cyphierion@users.noreply.github.com>
2018-09-26 19:34:21 -0400

    Add files via upload

---
+++ U3XTools/U3XInit.cs
@@ -0,0 +1,35 @@
+﻿
+// C# System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// RocketMod for Unturned 3.X.X.X
+using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+
+// Unturned + UnityEngine 5
+using SDG.Unturned;
+using UnityEngine;
+
+namespace U3XTools
+{
+    public class U3XInit : RocketPlugin<U3XConfig>
+    {
+        protected override void Load()
+        {
+
+        }
+
+        protected override void Unload()
+        {
+
+        }
+
+        public static U3XInit instance;
+    }
+}
