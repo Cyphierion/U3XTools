35f4f215d1819ee296a89fcd8d11f5394f20bbec
Cyphierion <35496928+Cyphierion@users.noreply.github.com>
2018-09-26 21:11:31 -0400

    Add files via upload

---
+++ U3XTools Project/U3XTools/U3XConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rocket.API;
+
+namespace U3XTools
+{
+    public class U3XConfig : IRocketPluginConfiguration, IDefaultable
+    {
+        public void LoadDefaults()
+        {
+
+        }
+    }
+}
